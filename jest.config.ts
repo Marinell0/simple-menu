0450c44dc136990d3b0f39e6dd5d6c06d34c8650
Gabriel Marinello Moura Leite <gabrielmarinello6@gmail.com>
2024-03-13 12:06:21 -0300

    Add and configure eslint

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,14 @@
-import type {Config} from 'jest';
+import type { Config } from "jest";
 
 export default async (): Promise<Config> => {
-  return {
-    cacheDirectory: '.jest-cache',
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageProvider: 'v8',
-    restoreMocks: true,
-    verbose: true,
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-  };
+	return {
+		cacheDirectory: ".jest-cache",
+		collectCoverage: true,
+		coverageDirectory: "coverage",
+		coverageProvider: "v8",
+		restoreMocks: true,
+		verbose: true,
+		preset: "ts-jest",
+		testEnvironment: "node",
+	};
 };
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,11 @@
 import { getHelloWorld } from "../src";
 
-describe('Test hello world', () => {
-  it('returns hello world', () => {
-    const expected = 'Hello, World!';
+describe("Test hello world", () => {
+	it("returns hello world", () => {
+		const expected = "Hello, World!";
 
-    const actual = getHelloWorld();
+		const actual = getHelloWorld();
 
-    expect(actual).toBe(expected);
-  });
+		expect(actual).toBe(expected);
+	});
 });
